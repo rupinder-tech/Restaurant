73ffc799f04ccc7da46b5ff29bcdefc9c78a4ebd
Singh, Rupinderjeet(rs830d) <rs830d@.att.com>
2021-09-30 18:03:27 +0530

    q

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/angular.io-example'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/angular.io-example/index.html'));
+res.sendFile(path.join(__dirname+'/dist/angular.io-example/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
