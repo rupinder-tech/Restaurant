bd32405e5f21c4c4b47249557f9d2f136ceafca3
Singh, Rupinderjeet(rs830d) <rs830d@.att.com>
2021-09-30 18:10:33 +0530

    qq

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular.io-example'));
+app.use(requireHTTPS);
+app.use(express.static('./dist/angular-heroku'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/angular.io-example/src/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
+);
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
